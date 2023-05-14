e865cdb579288b7415d609f97ef22945b8678c2b
Hiromichi Suzuki <jakephot@gmail.com>
2023-05-15 03:31:57 +0900

    fix

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 include_once('config.php');
 
 // `index.php?d=2023/01/01` にアクセスすると、`2023/01/01`にオプトインしたものとみなす
-$d = $_GET['d'];
-if ($d === null) {
+try {
+  $d = $_GET['d'];
+  $format = 'Y/m/d';
+  $date = DateTime::createFromFormat($format, $d, new DateTimeZone('Asia/Tokyo'));
+  $opt = $date->getTimestamp();
+} catch (Throwable $e) {
   header("Location: $smartLinkUrl");
+  exit;
 }
-$format = 'Y/m/d';
-$date = DateTime::createFromFormat($format, $d, new DateTimeZone('Asia/Tokyo'));
 
 // Make a Time Tuner Token (JWT)
 $jwtHeader = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
 $jwtHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($jwtHeader));
-$jwtPayload = json_encode(['opt' => $date->getTimestamp()]);
+$jwtPayload = json_encode(['opt' => $opt]);
 $jwtPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($jwtPayload));
 $jwtSignature = hash_hmac('sha256', $jwtHeader . "." . $jwtPayload, $jwtSecret, true);
 $jwtSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($jwtSignature));
