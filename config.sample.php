3a07e184a666027fa35361a1146251ad5ed2ecbb
Hiromichi Suzuki <jakephot@gmail.com>
2023-05-15 11:23:11 +0900

    fix

--- config.sample.php
+++ config.sample.php
@@ -1,4 +1,4 @@
 <?php
 $jwtSecret = '';
-$smartLinkUrl = 'https:example.ever.bz/relative';
-$smartLinkApiUrl = 'https:example.ever.bz/api/v1/my/smart_links/relative?callback=onNavigationLoaded';
+$smartLinkUrl = 'https://example.ever.bz/relative';
+$smartLinkApiUrl = 'https://example.ever.bz/api/v1/my/smart_links/relative?callback=onNavigationLoaded';
